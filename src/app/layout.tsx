ea7797b7be484a8869115c801da4ba9ac74fb664
Wesley Lisboa <weesleylisbooa@gmail.com>
2026-04-25 10:32:41 -0300

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "@/components/layout/session-provider";
 import AppLayout from "@/components/layout/app-layout";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           <AppLayout>
             {children}
           </AppLayout>
+          <Toaster theme="dark" position="bottom-right" richColors />
         </SessionProvider>
       </body>
     </html>
