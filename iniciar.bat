c11f6eb0e0090881eacc582444a8fbfdde0e2d74
Wesley Lisboa <weesleylisbooa@gmail.com>
2026-05-18 03:33:58 -0300

    Create iniciar.bat

---
+++ iniciar.bat
@@ -0,0 +1,7 @@
+@echo off
+title Iniciar Site CSBrasil
+echo =======================================
+echo Iniciando o servidor de desenvolvimento...
+echo =======================================
+npm run dev
+pause
